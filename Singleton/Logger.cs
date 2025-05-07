a7b6e1b5a52e842c8e99a5c82e7b011b1feff915
Samuel <Samuel@DESKTOP-U9R3TFU>
2025-05-07 01:14:07 -0300

    Padrão singleton Realizado

---
+++ Singleton/Logger.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public class Logger
+    {
+        public string Path { get; set; }
+        public static Logger? instance { get; set; } = null;
+        //priva a classe de ser instanciada
+        private Logger()
+        {
+            Path = Directory.GetCurrentDirectory() + @"\log.txt";
+        }
+
+        public static Logger Instance()
+        {
+            return instance = new Logger();
+        }
+
+        public void GravarLog(string mensagem)
+        {
+            File.WriteAllText(Path,mensagem);
+        }
+    }
+}
---
+++ Singleton/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Net.WebSockets;
+using Singleton;
+
+Console.WriteLine("Padrão de pattern Singleton!!");
+
+Logger.Instance().GravarLog("Testando...");
